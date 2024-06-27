fef82954a9d7ec7ddf1cd2dc73c285842acc8072
MichaelNgure <mikynjoro01@gmail.com>
2024-06-28 02:42:12 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function writeCards (arrays, event){
+    let newArray = []
+    for(const array of arrays){
+    let result = (`Thank you, ${array}, for the wonderful ${event} gift!`)
+    newArray.push(result)
+    }
+    return newArray
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+// const names = ["Charlie", "Samip", "Ali"]
+// for (const name of names){
+//     console.log(name)
+// }
+
+function countDown(){
+    let count = 10
+    while (count > -1){
+        console.log(count)
+        count --
+    }
+}
